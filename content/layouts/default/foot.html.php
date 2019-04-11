75ce5671b14429304150fee2200109799c877338
Alexander Morland <alkemann@users.noreply.github.com>
2019-04-11 21:12:01 +0200

    Update foot.html.php

--- content/layouts/default/foot.html.php
+++ content/layouts/default/foot.html.php
@@ -1,7 +1,7 @@
 		<br />
 		<hr />
 		<br />
-		<footer>Copyright .. nah  |  <a href="https://github.com/alkemann/stone_soup_cosplay/issues/new" target="_blank">Send bug report or feature request</a></footer>
+		<footer>Copyright .. nah  |  <a href="https://github.com/alkemann/stone_soup_academy/issues/new" target="_blank">Send bug report or feature request</a></footer>
 		</div>
 	</div>
 </div>
