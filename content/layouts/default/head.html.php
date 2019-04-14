a16a5c5d0295e9f552c2ab4adacccacf49b43800
RoGGa <robert_gagne@yahoo.com>
2019-04-13 20:19:46 -0400

    change tab icon to Snorg

--- content/layouts/default/head.html.php
+++ content/layouts/default/head.html.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Crawl Cosplay Academy</title>
-    <link rel="icon" href="/img/stone_soup_icon-32x32.png" type="image/png">
+    <link rel="icon" href="http://crawl.chaosforge.org/images/e/e8/Snorg.png type="image/png">
     <!-- <link rel="stylesheet" 	href="/css/reset.css"> -->
     <link rel="stylesheet" href="/css/cosplay.css?v=<?=time()?>">
     <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
