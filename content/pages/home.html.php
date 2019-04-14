1b986322a992089fbcac2acf399f98f67d317de0
RoGGa <robert_gagne@yahoo.com>
2019-04-14 14:06:15 -0400

    updated some gods

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -10,7 +10,7 @@
 <ul class="charlist">
     <li><img src="http://crawl.chaosforge.org/images/e/e8/Snorg.png"> <a href="#">Snorg, the troll berzerker of Trog</a></li>
     <li><img src="http://crawl.chaosforge.org/images/7/71/Asterion.png"> <a href="#">Asterion, the minotaur fighter of Makhleb</a></li>
-    <li><img src="http://crawl.chaosforge.org/images/5/52/Nessos.png"> <a href="#">Nessos, the Markscentaur    ...of god?</a></li>
+    <li><img src="http://crawl.chaosforge.org/images/5/52/Nessos.png"> <a href="#">Nessos, the hunter centaur of Okawaru</a></li>
     <li><img src="http://crawl.chaosforge.org/images/8/87/Saint_Roka.png"> <a href="#">Saint Roka, the Hill Orc Monk of Beogh</a></li>
-    <li><img src="http://crawl.chaosforge.org/images/a/a3/Grum.png"> <a href="#">Grum, the versatile gnoll and lover of polearms   God?</a></li>
+    <li><img src="http://crawl.chaosforge.org/images/a/a3/Grum.png"> <a href="#">Grum, the monk gnoll of Yredelemnul</a></li>
 </ul>
