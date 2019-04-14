a70a009a498a75869f74a1da4b4f99c3f66f7a3e
RoGGa <robert_gagne@yahoo.com>
2019-04-13 20:37:07 -0400

    add other icons from crawl wiki

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -7,9 +7,9 @@
 <h3>Select your character:</h3>
 
 <ul class="charlist">
-    <li><img src="http://crawl.chaosforge.org/images/e/e8/Snorg.png"><a href="#">Snorg, the troll berzerker of Trog</a></li>
-    <li><a href="#">Asterion, the minotaur fighter of Makhleb</a></li>
-    <li><a href="#">Nessos, the Markscentaur    ...of god?</a></li>
-    <li><a href="#">Saint Roka, the Hill Orc Monk of Beogh</a></li>
-    <li><a href="#">Grum, the versatile gnoll and lover of polearms   God?</a></li>
+    <li><img src="http://crawl.chaosforge.org/images/e/e8/Snorg.png"> <a href="#">Snorg, the troll berzerker of Trog</a></li>
+    <li><img src="http://crawl.chaosforge.org/images/7/71/Asterion.png"> <a href="#">Asterion, the minotaur fighter of Makhleb</a></li>
+    <li><img src="http://crawl.chaosforge.org/images/5/52/Nessos.png"> <a href="#">Nessos, the Markscentaur    ...of god?</a></li>
+    <li><img src="http://crawl.chaosforge.org/images/8/87/Saint_Roka.png"> <a href="#">Saint Roka, the Hill Orc Monk of Beogh</a></li>
+    <li><img src="http://crawl.chaosforge.org/images/a/a3/Grum.png"> <a href="#">Grum, the versatile gnoll and lover of polearms   God?</a></li>
 </ul>
