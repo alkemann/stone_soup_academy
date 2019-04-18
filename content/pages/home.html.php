6f055601303461629815abee03f8f3af8a9cb653
RoGGa <robert_gagne@yahoo.com>
2019-04-18 18:20:14 -0400

    edited and added to homepage

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -5,12 +5,16 @@
 <p><b>Our approach</b>: play one of five beginner level combos, each based on a Crawl Unique.</p> 
 <p>              You'll be earning points by following specific conducts that help you develop good habits.</p>
 
-<h3>Select your character:</h3>
+<h3>Select your character guide:</h3>
 
 <ul class="charlist">
     <li><img src="./img/Snorg.png"> <a href="#">Snorg, the troll berzerker of Trog</a></li>
     <li><img src="./img/Asterion.png"> <a href="#">Asterion, the minotaur fighter of Makhleb</a></li>
-    <li><img src="./img/Nessos.png"> <a href="#">Nessos, the hunter centaur of Okawaru</a></li>
+    <li><img src="./img/Nessos.png"> <a href="#">Nessos, the centaur hunter of Okawaru</a></li>
     <li><img src="./img/Saint_Roka.png"> <a href="#">Saint Roka, the Hill Orc Monk of Beogh</a></li>
-    <li><img src="./img/Grum.png"> <a href="#">Grum, the monk gnoll of Yredelemnul</a></li>
+    <li><img src="./img/Grum.png"> <a href="#">Grum, the gnoll monk of Yredelemnul</a></li>
 </ul>
+
+For tactical guides, see:
+    <li><a href="http://www.ultraviolent4.com/guide.html#General_Tactics">UV4's General Tactics</a></li>
+    <li><a href="http://www.ultraviolent4.com/patashutactics.html">Patashu's Crawl Tactics Guide</a></li>
