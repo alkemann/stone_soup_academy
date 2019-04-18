b522d1b66465a59f0e492ced942295f1f2b80cbf
RoGGa <robert_gagne@yahoo.com>
2019-04-18 18:05:14 -0400

    update path of dolls to local img folder

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -9,8 +9,8 @@
 
 <ul class="charlist">
     <li><img src="./img/Snorg.png"> <a href="#">Snorg, the troll berzerker of Trog</a></li>
-    <li><img src="http://crawl.chaosforge.org/images/7/71/Asterion.png"> <a href="#">Asterion, the minotaur fighter of Makhleb</a></li>
-    <li><img src="http://crawl.chaosforge.org/images/5/52/Nessos.png"> <a href="#">Nessos, the hunter centaur of Okawaru</a></li>
-    <li><img src="http://crawl.chaosforge.org/images/8/87/Saint_Roka.png"> <a href="#">Saint Roka, the Hill Orc Monk of Beogh</a></li>
-    <li><img src="http://crawl.chaosforge.org/images/a/a3/Grum.png"> <a href="#">Grum, the monk gnoll of Yredelemnul</a></li>
+    <li><img src="./img/Asterion.png"> <a href="#">Asterion, the minotaur fighter of Makhleb</a></li>
+    <li><img src="./img/Nessos.png"> <a href="#">Nessos, the hunter centaur of Okawaru</a></li>
+    <li><img src="./img/Saint_Roka.png"> <a href="#">Saint Roka, the Hill Orc Monk of Beogh</a></li>
+    <li><img src="./img/Grum.png"> <a href="#">Grum, the monk gnoll of Yredelemnul</a></li>
 </ul>
