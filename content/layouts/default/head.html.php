e157457f9237e4ecffba7f7e3fcbd430da13f605
RoGGa <robert_gagne@yahoo.com>
2019-04-18 18:10:05 -0400

    add missing quote

--- content/layouts/default/head.html.php
+++ content/layouts/default/head.html.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Crawl Cosplay Academy</title>
-    <link rel="icon" href="./img/Snorg.png type="image/png">
+    <link rel="icon" href="./img/Snorg.png" type="image/png">
     <!-- <link rel="stylesheet" 	href="/css/reset.css"> -->
     <link rel="stylesheet" href="/css/cosplay.css?v=<?=time()?>">
     <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
