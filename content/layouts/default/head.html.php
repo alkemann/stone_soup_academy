f693ffe912a172645c83ff03d86db76e141efd18
RoGGa <robert_gagne@yahoo.com>
2019-04-18 18:06:50 -0400

    update browser's tab icon to local Snorg img

--- content/layouts/default/head.html.php
+++ content/layouts/default/head.html.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Crawl Cosplay Academy</title>
-    <link rel="icon" href="https://cdn.discordapp.com/attachments/558269304852709393/568513951328108584/Snorg.png type="image/png">
+    <link rel="icon" href="./img/Snorg.png type="image/png">
     <!-- <link rel="stylesheet" 	href="/css/reset.css"> -->
     <link rel="stylesheet" href="/css/cosplay.css?v=<?=time()?>">
     <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
