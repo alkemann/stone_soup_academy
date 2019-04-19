4eaa341dd89d483596bfe594362096f5c0dd3928
RoGGa <robert_gagne@yahoo.com>
2019-04-19 18:53:21 -0400

    Update scoring.html.php

--- content/pages/scoring.html.php
+++ content/pages/scoring.html.php
@@ -5,5 +5,5 @@
 	<tr><td>Collect 3 runes</td><td>2<td> 6</td></tr>
 	<tr><td>Collect Orb of Zot</td><td>2<td> 2</td></tr>
 	<tr><td>Win/Escape</td><td>3<td> 3</td></tr>
-	<tr><td>Total</td><td><td>50</td></tr>
+	<tr><th>Total</th><th><th>50</th></tr>
 </table>
