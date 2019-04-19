9393a8bf8e584b77525e72cdc15c40cba926bd6b
RoGGa <robert_gagne@yahoo.com>
2019-04-19 18:07:04 -0400

    Update snorg.html.php

--- content/pages/uniques/snorg.html.php
+++ content/pages/uniques/snorg.html.php
@@ -1,1 +1,12 @@
-<h1>Snorg's Character Guide</h1>
+<h3>Snorg's Character Guide</h3>
+
+Snorg, 
+		<td></td><td>a particularly corpulent troll with a short temper and a sizable gut. Uninterested in the Orb, 
+                 Snorg delights in the endless parade of adventurers – that is, easy meals — it attracts.</td><td></td>
+
+As a <a href="http://crawl.chaosforge.org/Troll">Troll</a> <a href="http://crawl.chaosforge.org/Berserker">Berzerker</a> (TrBe), you'll be
+starting with only 3 levels of INT. This isn't much of an issue during the early game but could require some INT investment come mid-game 
+if no equipment of jewelery boost your INT.
+
+add: How to deal with Hydras!!!
+	
