a2f3122a2055b6fc84b19059c038dca3f9c36241
RoGGa <robert_gagne@yahoo.com>
2019-04-19 14:59:17 -0400

    minor update to homepage: shortened links to Uniques

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -2,16 +2,16 @@
 <h2>Welcome</h2>
 <p>Simply put, the goal is to provide you with a structured approach to ease the learning curve while playing characters based on Uniques.</p>
 <p>The 5 characters below are considered to be "easier" as they do not rely on the use of magic.</p>
-<p>Once you have finished a game, submit your morque for scoring. This will allow you to be added to the scoreboard so that</p> 
-<p>you can track your progress and also compare yourself to others.</p>
+<p>Once you have finished a game, submit your morque for scoring. This will allow you to be added to the scoreboard so that you can track 
+    your progress and also compare yourself to others.</p>
 <p>If you are fairly new to DCSS, the following character guides should be very helpful.</p>
 <h3>Guides for "Simple" Characters:</h3>
 <ul class="charlist">
-    <li><img src="./img/Snorg.png"> <a href="#">Snorg, the <b>Tr</b>oll <b>Be</b>rzerker of Trog</a></li>
-    <li><img src="./img/Asterion.png"> <a href="#">Asterion, the <b>Mi</b>notaur <b>Fi</b>ghter of Makhleb</a></li>
-    <li><img src="./img/Nessos.png"> <a href="#">Nessos, the <b>Ce</b>ntaur <b>Hu<b>nter of Okawaru</a></li>
-    <li><img src="./img/Saint_Roka.png"> <a href="#">Saint Roka, the <b>H</b>ill <b>O</b>rc <b>Mo<b>nk of Beogh</a></li>
-    <li><img src="./img/Grum.png"> <a href="#">Grum, the <b>Gn</b>oll <b>Gl</b>adiator of Yredelemnul</a></li>
+    <li><img src="./img/Snorg.png"> <a href="#">Snorg</a>, the <b>Tr</b>oll <b>Be</b>rzerker of Trog</li>
+    <li><img src="./img/Asterion.png"> <a href="#">Asterion</a>, the <b>Mi</b>notaur <b>Fi</b>ghter of Makhleb</li>
+    <li><img src="./img/Nessos.png"> <a href="#">Nessos</a>, the <b>Ce</b>ntaur <b>Hu<b>nter of Okawaru</li>
+    <li><img src="./img/Saint_Roka.png"> <a href="#">Saint Roka</a>, the <b>H</b>ill <b>O</b>rc <b>Mo<b>nk of Beogh</li>
+    <li><img src="./img/Grum.png"> <a href="#">Grum</a>, the <b>Gn</b>oll <b>Gl</b>adiator of Yredelemnul</li>
 </ul>
 
 For tactical guides, see:
