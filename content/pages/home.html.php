908adbacc29bfd6f9bcd8d43baaa767ff2976dbc
RoGGa <robert_gagne@yahoo.com>
2019-04-19 18:32:54 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -10,7 +10,7 @@ The 5 characters below are considered to be "easier" as they do not rely on the 
     <li><img src="/img/Snorg.png"> <a href="/uniques/snorg">Snorg</a>, the <b>Tr</b>oll <b>Be</b>rzerker (TrBe) of Trog</li>
     <li><img src="/img/Asterion.png"> <a href="/uniques/asterion">Asterion</a>, the <b>Mi</b>notaur <b>Fi</b>ghter (MiFi) of Makhleb</li>
     <li><img src="/img/Nessos.png"> <a href="/uniques/nessos">Nessos</a>, the <b>Ce</b>ntaur <b>Hu</b>nter of (CeHu) Okawaru</li>
-    <li><img src="/img/Saint_Roka.png"> <a href="/uniques/saint-roka">Saint Roka</a>, the <b>H</b>ill <b>O</b>rc <b>Mo<b>nk (HOMo) of Beogh</li>
+    <li><img src="/img/Saint_Roka.png"> <a href="/uniques/saint-roka">Saint Roka</a>, the <b>H</b>ill <b>O</b>rc <b>Mo</b>nk (HOMo) of Beogh</li>
     <li><img src="/img/Grum.png"> <a href="/uniques/grum">Grum</a>, the <b>Gn</b>oll <b>Gl</b>adiator (GnGl) of Yredelemnul</li>
 </ul>
 
