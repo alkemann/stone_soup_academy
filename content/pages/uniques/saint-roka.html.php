9589c5b52166c32f5a1965da396d152ccd67d688
RoGGa <robert_gagne@yahoo.com>
2019-04-19 19:38:27 -0400

    Update saint-roka.html.php

--- content/pages/uniques/saint-roka.html.php
+++ content/pages/uniques/saint-roka.html.php
@@ -6,11 +6,112 @@
 <img src="/img/HR-right.png"><br />
 
 <h2>Character Guide</h2>
-by <a href="http://www.ultraviolent4.com/authors/ultraviolent4.html">Ultraviolent4</a>
-
-Difficulty: Simple
-<a href="https://www.youtube.com/playlist?list=PLaA1ahtMIRbMfV4qyLj8h2f1ZIuOYNg8_">Full HOMo of Beogh Playlist</a>
-<a href="https://www.youtube.com/playlist?list=PLaA1ahtMIRbO80iXtoiGFiyupMqmAFPWD">Full 15 Rune HOFi of Beogh Playlist</a>
-
-Posted: March 2, 2018
+by <a href="http://www.ultraviolent4.com/authors/ultraviolent4.html">Ultraviolent4</a><br>
+<br>
+Difficulty: Simple<br>
+<a href="https://www.youtube.com/playlist?list=PLaA1ahtMIRbMfV4qyLj8h2f1ZIuOYNg8_">Full HOMo of Beogh Playlist</a><br>
+<a href="https://www.youtube.com/playlist?list=PLaA1ahtMIRbO80iXtoiGFiyupMqmAFPWD">Full 15 Rune HOFi of Beogh Playlist</a><br>
+<br>
+Posted: March 2, 2018<br>
 Crawl Version: 0.21 + Trunk(v0.22)<p />
+
+<h3>Why play Hill Orc Monk of Beogh?</h3>
+Disclaimer: This is my absolute favourite species, background and god combination in all of Crawl.<br>
+<br>
+If the idea of a little roleplaying in your roguelike sounds appealing, then this is the choice for you.<br>
+<br>
+Beogh is the (so-called) "evil" god of the Hill Orcs who enjoys it when you kill non-orcs, apostates and blasphemers. 
+While preaching the word of Beogh, you can build fearsome armies of orcs by converting them to your side. 
+As your piety level and experience level (XL) rises, you're more likely to convince any orc you meet or bring close to death that you're the messiah. 
+If that happens, the orc will become a permanent follower and, as it gains experience fighting by your side, will level up.<br>
+<br>
+<br>
+As Hill Orc is the only species that can worship Beogh, this part of the combination is mandatory.
+Monk starts with a weapon of your choice which is nice for a Hill Orc who has a +3 aptitude in Axes. 
+The main feature of the Monk background, however, is that you get ** of piety immediately on worshiping a god. 
+Not only does this allow you access to Beogh's first ability (Smiting) right away, it gives you a running start for any 
+army-building ambitions.<br>
+<br>
+Challenges to Consider
+Worshiping Beogh in the first place
+
+Worshiping Beogh usually works differently than any other god. While there are shrines you can kneel at, you usually won't find them outside of the Orcish Mines (and certainly not in the Temple).
+
+The standard way to worship is to find an orc priest. When it notices you, you'll be given a new ability which you can access from the ability menu with a then Y (Shift + y). Any orcs who see you convert will turn from hostile to neutral.
+
+Getting to ** and *** of piety
+
+While Beogh ultimately becomes an extremely powerful god, the start is a little slow. Indeed, at * of piety you get no benefits at all. From ** you have access to Smiting and *** allows you to start converting followers and building an army.
+
+Fortunately, the Monk background helps by catapulting you straight to **. Also, as a Hill Orc with an axe, you're relatively strong on your own in the early game. Still, don't be afraid to use wand charges, potions and other consumables to get you through the start: it will be the most challenging time.
+
+Keeping your followers alive
+
+Unless you're some sort of maniac, you don't want the orcs in your charge to die. When given the choice between attacking your character or a follower, monsters will typically choose you. As such, the best way to keep your orcs alive is to stand at the front and tank for the group.
+
+I highly recommend the largest shield you can find on a Beogh character because that will make you much more effective at tanking. And since your followers will always add additional damage output, you don't need to worry about the lower base damage of a one-handed weapon.
+
+If in doubt, remember that the messiah gig requires you to die for the sins of your followers.
+
+Micromanagement
+
+It's not uncommon to hear people say Beogh is tedious to play due to the follower micromanagement required.
+
+While it's true that some management is ideal, it's not really too much and a bit of practice makes it straightforward. Some tips follow later. Still, if the idea of any micromanagement at all freaks you out, you might want to consider a different character.
+
+
+
+Early Skilling
+Your early-game skilling for a Hill Orc Monk should look similar to any melee brute character. Focus your weapon (which is usually an axe due to Hill Orc's +3 Axes aptitude) as it's the most important skill initially. I also like to train Fighting and Armour for the entire game.
+
+If you ever find a shield (and you haven't already acquired an amazing two-handed weapon), train Shields.
+
+As soon as you start worshiping Beogh, turn on Invocations and train that until the end. My personal preference isn't to train Evocations but that's always an option if you find a strong early wand such as a wand of paralysis.
+
+Once my weapon reaches the skill required for mindelay (e.g. 18 for a broad axe) I like to focus Fighting for more HP and start training Dodging.
+
+Appropriate Gods
+People sometimes ask me for advice on a Hill Orc character that isn't worshiping Beogh. Whenever that happens I feel physically ill. You wouldn't want me to suffer, would you?
+
+All joking aside, if you don't follow Beogh on your Hill Orc then you're a heretic who should be ready to drown in a sea of blood.
+
+General Approach to the Run
+Your first mission is to survive the early Dungeon and find an orc priest so you can convert. If you're lucky that can be as early as D3.
+
+A Monk starts in robes so you want to upgrade to a heavier armour as soon as possible. Generally speaking, this character wants to wear the heaviest armour it can find and, to that end, wants to choose Strength as its stat on level-ups.
+
+Once you have Beogh, try not to use Smiting too much until you get to *** of piety which is when you can start converting (feel free to go nuts once you do have some orcs). I don't usually head into Lair until I have at least 2 regular orc followers because they will gain experience there and become orc knights. Orc knights can might each other (150% damage for monsters) so a pair of them is extremely powerful during the midgame.
+
+Orcs will eventually become named as they gain experience and level up. From ***** you get the ability to gift items to named followers. By far, the best thing to give them is a polearm. This is because the reaching property allows an orc to attack even when it's standing behind you or another orc. If a follower already has a one-handed polearm, give them a shield. And if they already have a two-handed polearm, you can go with a ranged weapon or magical armour.
+
+You can run around with all of your army at once or train up smaller groups until they reach orc warlord/sorcerer/high priest. The latter requires a bit more care but you'll ultimately end up with a larger and stronger force. Don't worry too much about trying to keep orc priests and wizards alive because it's very difficult to do (and impossible if a wizard has Blink). If one of your important orcs dies and leaves a corpse, you can resurrect it at ****** (full piety).
+
+I like to hold off on doing the Orcish Mines until I have max piety. Not only does that give you the highest chance to convert followers, it also allows you to resurrect any dead warlords you find.
+
+As far as S branches go, there isn't a huge difference between Snake and Spider. Shoals is much more desirable to Swamp, however. From ***** of piety, Beogh allows you to walk on water which removes one major Shoals difficulty. You're also going to confiscate many branded polearms from Merfolk which can then go into your arms chest. Swamp is extremely dangerous for your followers because they can be drowned by the noxious gas from swamp drakes. You shouldn't bring an orc in with you unless they have rPois. I repeat: DO NOT BRING FOLLOWERS INTO SWAMP.
+
+For a 3rd rune, you almost always want to do Vaults 5. By parking your followers at top of the stairs on V4 you can stair dance while smiting any vault wardens that lock you in. See how it's done here.
+
+Bring your whole army with you into Zot and smite any orbs of fire, curse toes and ancient liches on sight.
+
+Other Notes, Tips and Tricks
+Managing your followers
+
+The main difficulty when managing your followers is separating out specific orcs to train. There are two very consistent ways to do this and they both rely on t to issue commands.
+
+For "recall herding" begin by using the recall ability. Each turn, starting with your strongest followers, a couple of orcs will appear. Press tg every turn to tell the arrivals to guard the area. When the orc you want is recalled, stop the recall by using the ability again. As every orc except for the newest to arrive has been told to guard the area, only that one or two will then follow you.
+
+
+"Retreat herding" uses tr to retreat orcs away from you. As warlords have a higher hit dice (HD) than any other orcs, they tend to push their way to the front when retreating. Once they are off the screen and can no longer hear you, tell the other orcs to follow you again with tf.
+
+
+Miscellaneous
+
+Smiting is an amazing ability. It does insane (if you've trained Invocations) amounts of AC-ignoring damage, it can hit anywhere on screen and it can't miss. 
+Don't be afraid to spam the ability. In my most recent Hill Orc game, I used Smiting 191 times.
+You can step behind followers to break line of fire to uniques and monsters with dangerous hexes to avoid being banished or paralysed. Combo this with Smiting for true streak-player(tm) gameplay.
+In a dangerous situation you can recall all your followers to you. Even if you're just trying to escape, many angry orcs will provide a good distraction.
+You can temporarily retreat orcs away from you when facing monsters with large amounts of AoE such as death drakes or fire giants. Just remember to tell them to follow you again before you rest so they don't wander off.
+Stair dancing is extremely broken with permanent followers if you first tell them to guard the top of the stairs. Monsters will die without being able to act as they ascend.
+Orcs can kind of fight when you can't see them but not very well. If you need to teleport away, you should recall your followers back to you so they don't die.
+If you have full piety you can resurrect an orc and it will be brought back to life as a follower. This can revive one of your fallen comrades but it will also work on an orc that wasn't converted - effectively giving you another chance to get a powerful orc such as Saint Roka.
