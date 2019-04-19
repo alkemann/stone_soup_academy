9279d7e5396f1334713f442d382d7ca704534355
RoGGa <robert_gagne@yahoo.com>
2019-04-19 19:04:22 -0400

    Update snorg.html.php

--- content/pages/uniques/snorg.html.php
+++ content/pages/uniques/snorg.html.php
@@ -7,9 +7,9 @@
 <img src="/img/HR-right.png"><br />
 
 <h2>Character Guide</h2>
-<p>As a <a href="http://crawl.chaosforge.org/Troll">Troll</a> <a href="http://crawl.chaosforge.org/Berserker">Berzerker</a> (TrBe), you'll be
-starting with only 3 levels of INT. This isn't much of an issue during the early game but could require some INT investment come mid-game 
-if no equipment or jewelery boosts your INT.</p>
+<p>As a <a href="http://crawl.chaosforge.org/Troll">Troll</a> <a href="http://crawl.chaosforge.org/Berserker">Berzerker</a> (TrBe),
+	you'll be starting with only 3 levels of INT. This isn't much of an issue during the early game but could require some INT 
+	investment come mid-game if no equipment or jewelery boosts your INT.</p>
 <p />
 add: How to deal with Hydras!!!
 <p />
