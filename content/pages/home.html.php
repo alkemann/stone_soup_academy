598104fb04380e4d8df8111e642dd213bbf922e7
RoGGa <robert_gagne@yahoo.com>
2019-04-20 15:12:42 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -1,12 +1,14 @@
 <h2>Welcome "Weakling" *</h2>
 <h4>Do you have dreams of becoming a "Journeyman" or even an "Adept" crawler?</h4>
-<p>First, you'll need to win a game to get the title of "Amateur" and then a 2nd win to join the <a href="/ranking-system">ranks</a> of "Novice". 
+<p>First, you'll need to win a game to get the title of "Amateur" and then a 2nd win to join the 
+    <a href="/ranking-system">ranks</a> of "Novice". 
 <p>But fear not, your dream is the goal of the Cosplay Academy:
-<li>To provide you with a structured approach to ease the learning curve while playing fun characters based on <a href="http://crawl.chaosforge.org/Unique_monster">DCSS Uniques</a>;</li> 
+<li>To provide you with a structured approach to ease the learning curve while playing fun characters based on 
+    <a href="http://crawl.chaosforge.org/Unique_monster">DCSS Uniques</a>.</li> 
 <p>This will also allow you to:</p>
     <li>better compete in the <a href="http://www.crawlcosplay.com/">weekly Crawl Cosplay Challenge</a>.</li>
 <p />
-<h4>Your first Academy goal</h4>
+<h4>Your first Academy goal...</h4>
 ...is to play one of the "melee brutes" listed below since they are considered to be <b><i>easier</i></b>.</p>
 <p>Once you have won a game, <a href="/submit">Submit your Morque</a> to gain access to your next Unique's mission</a>. 
     You'll also be added to the <a href="/scoreboard">Honour Roll</a> allowing you to see also the accomplishment of other crawlers.</p>
@@ -17,7 +19,10 @@
     <li><img src="/img/Asterion.png"> <a href="/uniques/asterion">Asterion</a>, the <u><b>Mi</b></u>notaur <u><b>Fi</b></u>ghter (MiFi) of Makhleb</li>
     <li><img src="/img/Saint_Roka.png"> <a href="/uniques/saint-roka">Saint Roka</a>, the <u><b>H</b></u>ill <u><b>O</b></u>rc <u><b>Mo</b></u>nk (HOMo) of Beogh (beginner-intermediate level)</li>
 </ul>
-<h4>For tactical guides:</h4>
+<h3>Need more help?</h3>
+<p>Come chat with us on our <a href="https://discord.gg/ZQ4kk6n">CosPlay Discord server</a> in the #cosplay-academy text channel.
+<p>Want to do a bit of the legwork on your own, then read the following:
+<h4>Tactical guides:</h4>
     <li><a href="http://www.ultraviolent4.com/guide.html#General_Tactics">UV4's General Tactics</a></li>
     <li><a href="http://www.ultraviolent4.com/patashutactics.html">Patashu's Crawl Tactics Guide</a></li>
 <p />
