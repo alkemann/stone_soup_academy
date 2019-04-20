9a85f77febe971d200134459a57418d0c2214e66
RoGGa <robert_gagne@yahoo.com>
2019-04-19 20:42:52 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -2,7 +2,7 @@
 <h2>Welcome</h2>
 <p>The goal of Cosplay Academy is to provide you with a structured approach to ease the learning curve while playing characters based on 
     Uniques. The characters listed below are considered to be "easier" as they do not rely on the use of magic during the early game. 
-    (Other character sets will be developed at a later date.) </p>
+    (Other character sets will be developed at a later date.) Also, the assigned Gods are considered part of the better ones.</p>
 <p>Once you have finished a game, <a href="/submit">Submit your Morque</a> for <a href="/scoring">Scoring</a>. 
     This will allow you to be added to the <a href="/scoreboard">Scoreboard</a> so that you can track your progress and also compare 
     yourself to others.</p>
@@ -11,11 +11,11 @@
 <ul class="charlist">
     <li><img src="/img/Snorg.png"> <a href="/uniques/snorg">Snorg</a>, the <b>Tr</b>oll <b>Be</b>rzerker (TrBe) of Trog</li>
     <li><img src="/img/Asterion.png"> <a href="/uniques/asterion">Asterion</a>, the <b>Mi</b>notaur <b>Fi</b>ghter (MiFi) of Makhleb</li>
-    <li><img src="/img/Nessos.png"> <a href="/uniques/nessos">Nessos</a>, the <b>Ce</b>ntaur <b>Hu</b>nter of (CeHu) Okawaru</li>
     <li><img src="/img/Saint_Roka.png"> <a href="/uniques/saint-roka">Saint Roka</a>, the <b>H</b>ill <b>O</b>rc <b>Mo</b>nk (HOMo) of Beogh</li>
+    <li><img src="/img/Nessos.png"> <a href="/uniques/nessos">Nessos</a>, the <b>Ce</b>ntaur <b>Hu</b>nter (CeHu) of Okawaru</li>
     <li><img src="/img/Grum.png"> <a href="/uniques/grum">Grum</a>, the <b>Gn</b>oll <b>Gl</b>adiator (GnGl) of Yredelemnul</li>
 </ul>
 
-For tactical guides, see:
+<h4>For tactical guides:</h4>
     <li><a href="http://www.ultraviolent4.com/guide.html#General_Tactics">UV4's General Tactics</a></li>
     <li><a href="http://www.ultraviolent4.com/patashutactics.html">Patashu's Crawl Tactics Guide</a></li>
