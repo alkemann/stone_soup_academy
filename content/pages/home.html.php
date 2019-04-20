ceff962e4540253571c7bcaa2a01427a0f290e05
RoGGa <robert_gagne@yahoo.com>
2019-04-20 14:41:52 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -3,22 +3,20 @@
 <p>First, you'll need to win a game to get the title of "Amateur" and then a 2nd win to join the ranks of "Novice". 
 <p>But fear not, your dream is the goal of the Cosplay Academy:
 <li>To provide you with a structured approach to ease the learning curve while playing fun characters based on Uniques;</li> 
-This will also allow you to:
+<p>This will also allow you to:</p>
     <li>better compete in the <a href="http://www.crawlcosplay.com/">weekly Crawl Cosplay Challenge.</a></li>
 <p />
 <h4>Your first Academy goal</h4>
-...is to play a melee brute since they are considered to be "easier".</p>
-<p>Once you have finished a game, <a href="/submit">Submit your Morque</a> for <a href="/scoring">Scoring</a>. 
-    This will allow you to be added to the <a href="/scoreboard">Scoreboard</a>(as an 
-    <i><a href="https://crawl.kelbi.org/morgue-test/best-players-total-score.html">example</a></i>) so that you can track your progress 
-    and also compare yours to others'.</p>
+...is to play one of the "melee brutes" below since they are considered to be "easier".</p>
+<p>Once you have won a game, <a href="/submit">Submit your Morque</a> to get access to your next mission</a>. 
+    You'll also be added to the <a href="/scoreboard">Honour Roll</a> allowing you to see also the accomplishment of other crawlers.</p>
 <p>If you are fairly new to DCSS, the following character guides should be very helpful.</p>
-<h3>Guides for "Simple" Characters:</h3>
+<h3>Guides for "Simple" melee characters:</h3>
 <ul class="charlist">
     <li><img src="/img/Snorg.png"> <a href="/uniques/snorg">Snorg</a>, the <u><b>Tr</b></u>oll <u><b>Be</b></u>rzerker (TrBe) of Trog</li>
     <li><img src="/img/Asterion.png"> <a href="/uniques/asterion">Asterion</a>, the <u><b>Mi</b></u>notaur <u><b>Fi</b></u>ghter (MiFi) of Makhleb</li>
     <li><img src="/img/Saint_Roka.png"> <a href="/uniques/saint-roka">Saint Roka</a>, the <u><b>H</b></u>ill <u><b>O</b></u>rc <u><b>Mo</b></u>nk (HOMo) of Beogh</li>
-</ul>
+</ul> (intermediate)
 <h4>For tactical guides:</h4>
     <li><a href="http://www.ultraviolent4.com/guide.html#General_Tactics">UV4's General Tactics</a></li>
     <li><a href="http://www.ultraviolent4.com/patashutactics.html">Patashu's Crawl Tactics Guide</a></li>
