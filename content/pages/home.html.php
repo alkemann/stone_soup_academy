7ef7ebf087d9e8214dfec94588d299381b1cefe5
RoGGa <robert_gagne@yahoo.com>
2019-04-20 14:31:34 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -1,12 +1,13 @@
 <h2>Welcome "Weakling"</h2>
-<p>Do you have dreams of becoming a "Journeyman" or even an "Adept" crawler? You'll first need to win a game to get the title of "Amateur"
-and then a 2nd win to join the ranks of "Novice". But fear not, your dream is the goal of the Cosplay Academy to:
-<li>provide you with a structured approach to ease the learning curve while playing characters based on Uniques;</li> 
-in order to
+<h4>>Do you have dreams of becoming a "Journeyman" or even an "Adept" crawler?</h4>
+<p>First, you'll need to win a game to get the title of "Amateur" and then a 2nd win to join the ranks of "Novice". 
+<p>But fear not, your dream is the goal of the Cosplay Academy:
+<li>To provide you with a structured approach to ease the learning curve while playing fun characters based on Uniques;</li> 
+This will also allow you to:
     <li>better compete in the <a href="http://www.crawlcosplay.com/">weekly Crawl Cosplay Challenge.</a></li>
-
-<p>Your first Academy goal is to play a melee brute since they are considered to be "easier". 
-    (Other character sets will be developed at a later date.) Also, the assigned Gods are considered part of the better ones.</p>
+<p />
+<h4>Your first Academy goal<h4>
+...is to play a melee brute since they are considered to be "easier".</p>
 <p>Once you have finished a game, <a href="/submit">Submit your Morque</a> for <a href="/scoring">Scoring</a>. 
     This will allow you to be added to the <a href="/scoreboard">Scoreboard</a>(as an 
     <i><a href="https://crawl.kelbi.org/morgue-test/best-players-total-score.html">example</a></i>) so that you can track your progress 
