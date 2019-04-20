cfa0d6d026827c4194e1281ae8fd81713167c9e4
RoGGa <robert_gagne@yahoo.com>
2019-04-20 11:15:35 -0400

    added ranking system

--- content/pages/scoring.html.php
+++ content/pages/scoring.html.php
@@ -1,3 +1,7 @@
+Currently being brainstormed<p />
+<p />
+Possible scoring grid for scoring individual games<p />
+<p />
 <table>
 	<tr><th>Milestones</th><th>Pts<th>Max pts</th></tr>
 	<tr><td>XL (Experience Level)</td><td>1<td>26</td></tr>
@@ -7,3 +11,14 @@
 	<tr><td>Win/Escape</td><td>3<td> 3</td></tr>
 	<tr><th>Total</th><th><th>50</th></tr>
 </table>
+<p />
+Ranking System based on Ghost Player ranks:<p />
+   "weakling",<p />
+    "amateur",    "novice",    "journeyman",    "adept",<p />
+    "veteran",    "master",    "legendary"<p />
+
+The player starts as a "weakling".<p />
+After their first melee style win, they get the level "amateur"<p />
+Novice is for a win with a Ranged-style unique<p />
+Journeyman is for a win with a hybrid-style unique<p />
+Adept is for a win with a mage-style unique<p />
