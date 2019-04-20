c61e8f84ccb006a50510981ce650b4b306f23e0c
RoGGa <robert_gagne@yahoo.com>
2019-04-20 14:22:19 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -3,7 +3,7 @@
 and then a 2nd win to join the ranks of "Novice". But fear not, your dream is the goal of the Cosplay Academy to:
 <li>provide you with a structured approach to ease the learning curve while playing characters based on Uniques;</li> 
 in order to
-    <li>better compete in the <a href="http://www.crawlcosplay.com/">weekly Crawl Cosplay Challenge.</li>
+    <li>better compete in the <a href="http://www.crawlcosplay.com/">weekly Crawl Cosplay Challenge.</a></li>
 
 <p>Your first Academy goal is to play a melee brute since they are considered to be "easier". 
     (Other character sets will be developed at a later date.) Also, the assigned Gods are considered part of the better ones.</p>
@@ -17,8 +17,6 @@ in order to
     <li><img src="/img/Snorg.png"> <a href="/uniques/snorg">Snorg</a>, the <u><b>Tr</b></u>oll <u><b>Be</b></u>rzerker (TrBe) of Trog</li>
     <li><img src="/img/Asterion.png"> <a href="/uniques/asterion">Asterion</a>, the <u><b>Mi</b></u>notaur <u><b>Fi</b></u>ghter (MiFi) of Makhleb</li>
     <li><img src="/img/Saint_Roka.png"> <a href="/uniques/saint-roka">Saint Roka</a>, the <u><b>H</b></u>ill <u><b>O</b></u>rc <u><b>Mo</b></u>nk (HOMo) of Beogh</li>
-    <li><img src="/img/Nessos.png"> <a href="/uniques/nessos">Nessos</a>, the <u><b>Ce</b></u>ntaur <u><b>Hu</b></u>nter (CeHu) of Okawaru</li>
-    <li><img src="/img/Grum.png"> <a href="/uniques/grum">Grum</a>, the <u><b>Gn</b></u>oll <u><b>Gl</b></u>adiator (GnGl) of Yredelemnul</li>
 </ul>
 <h4>For tactical guides:</h4>
     <li><a href="http://www.ultraviolent4.com/guide.html#General_Tactics">UV4's General Tactics</a></li>
