7c83ac4194e466a84c863998ad7ea221c384c1fd
RoGGa <robert_gagne@yahoo.com>
2019-04-21 17:58:35 -0400

    Update neck.html.php

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -1,12 +1,12 @@
 <div id="top-wall">&nbsp;</div>
 <div class="page information">
     <div class="toplinks"> 
-        <pre><a href="/">Home</a> | <a href="/about">About</a> | <a href="/ranking-system">Ranking System</a> | <a href="/submit">Submit a Morgue</a> | <a href="/honour-roll">Honour Roll</a> | <a href="/discord">Discord server</a>
+        <pre><a href="/">Home</a> | <a href="/about">About</a> | <a href="/ranking-system">Ranking System</a> | <a href="/submit">Submit a Morgue</a> | <a href="/honour-roll">Honour Roll</a> | <a href="/discord">Discord server</a></pre>
     </div>
     <div class="page_content">
         <div class="heading">
-            <h1><a href="/">Crawl Cosplay Academy</a></h1>
-            <div class="fineprint">Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Discord server</a></div></pre>
+            <h1><a href="/"><pre>Crawl Cosplay Academy</pre></a></h1>
         </div>
+        <br />
         <img src="/img/HR-right.png"><br />
     <div class="content">
