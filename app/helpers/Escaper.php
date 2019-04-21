8356969fff0c3667ba523a1c116b738433e903c8
Alexander Morland <alek.morland@gmail.com>
2019-04-21 22:49:28 +0200

    Added Escaper from Cosplay

---
+++ app/helpers/Escaper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace app\helpers;
+
+class Escaper
+{
+	public static function export(): array
+	{
+		return ['e' => 'app\helpers\Escaper::escape', 'em' => 'app\helpers\Escaper::escapeWithMarkdown'];
+	}
+
+
+	public static function escape(?string $s): string
+	{
+		return is_string($s) ? htmlspecialchars($s) : '';
+	}
+
+	public static function escapeWithMarkdown(?string $s): string
+	{
+		if (empty($s)) return '';
+		$Parsedown = new \Parsedown();
+		$Parsedown->setMarkupEscaped(true);
+		$Parsedown->setSafeMode(true);
+		return $Parsedown->text($s);
+	}
+}
