3ff6e2693adae69e0eb0341a0cbf7dedb123deda
RoGGa <robert_gagne@yahoo.com>
2019-04-21 17:27:35 -0400

    Update snorg.html.php

--- content/pages/uniques/snorg.html.php
+++ content/pages/uniques/snorg.html.php
@@ -6,7 +6,7 @@
 <img src="/img/HR-right.png"><br />
 
 <h2>Character Guide</h2>
-<p>As a <a href="http://crawl.chaosforge.org/Troll">Troll</a> <a href="http://crawl.chaosforge.org/Berserker">Berzerker</a> (TrBe),
+<p>As a <a href="http://crawl.chaosforge.org/Troll">Troll</a> <a href="http://crawl.chaosforge.org/Berserker">Berzerker</a> (<abbr title="Troll Berzerker">TrBe</abbr>),
 	you'll be starting with only 3 levels of INT. This isn't much of an issue during the early game but could require some INT 
 	investment come mid-game if no equipment or jewelery boosts your INT.</p>
 <p />
