2c1b97d5dac8f4767b3ebb8f46e483f295b07545
RoGGa <robert_gagne@yahoo.com>
2019-04-21 17:33:00 -0400

    Update snorg.html.php

--- content/pages/uniques/snorg.html.php
+++ content/pages/uniques/snorg.html.php
@@ -1,4 +1,4 @@
-<h2><img src="/img/Snorg.png"><a href="http://crawl.chaosforge.org/Snorg">Snorg</a> the Insatiable</h2>
+<h2><img src="/img/Snorg.png"> <a href="http://crawl.chaosforge.org/Snorg">Snorg</a>, the Insatiable</h2>
 <pre>        <i>An exceptionally fat troll who will go berserk at the slightest provocation.</i></pre><br />
 <img src="/img/HR-right.png"><br />
 
