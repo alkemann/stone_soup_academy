0dce9be7d42748660fbed3c565bb783a6d78e152
RoGGa <robert_gagne@yahoo.com>
2019-04-21 17:46:02 -0400

    Update neck.html.php

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -5,7 +5,7 @@
     </div>
     <div class="page_content">
         <div class="heading">
-            <h1><a href="/">Crawl Cosplay Academy</a></h1>
+            <pre><h1><a href="/">Crawl Cosplay Academy</a></h1><pre>
             <pre><div class="fineprint">Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Discord server</a></div></pre>
         </div>
         <img src="/img/HR-right.png"><br />
