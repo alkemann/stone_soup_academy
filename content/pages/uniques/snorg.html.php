603dc5cd0c4e4f69c8a07db0f9466f6be17a658e
RoGGa <robert_gagne@yahoo.com>
2019-04-21 17:34:41 -0400

    Update snorg.html.php

--- content/pages/uniques/snorg.html.php
+++ content/pages/uniques/snorg.html.php
@@ -1,11 +1,11 @@
 <h2><img src="/img/Snorg.png"> <a href="http://crawl.chaosforge.org/Snorg">Snorg</a>, the Insatiable</h2>
-<pre>        <i>An exceptionally fat troll who will go berserk at the slightest provocation.</i></pre><br />
+<pre>            <i>An exceptionally fat troll who will go berserk at the slightest provocation.</i></pre><br />
 <img src="/img/HR-right.png"><br />
 
 <h2>Character Guide</h2>
-<p>As a <a href="http://crawl.chaosforge.org/Troll">Troll</a> <a href="http://crawl.chaosforge.org/Berserker">Berzerker</a> (<abbr title="Troll Berzerker">TrBe</abbr>),
-	you'll be starting with only 3 levels of INT. This isn't much of an issue during the early game but could require some INT 
-	investment come mid-game if no equipment or jewelery boosts your INT.</p>
+<p>As a <a href="http://crawl.chaosforge.org/Troll">Troll</a> <a href="http://crawl.chaosforge.org/Berserker">Berzerker</a>
+	(<abbr title="Troll Berzerker">TrBe</abbr>), you'll be starting with only 3 levels of INT. 
+	This isn't much of an issue during the early game but could require some INT investment come mid-game if no equipment or jewelery boosts your INT.</p>
 <br />
 add: How to deal with Hydras!!!
 <br />
