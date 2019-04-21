172e9eaee8b6d7628ab0b2a43e46e9a91a2a29ab
RoGGa <robert_gagne@yahoo.com>
2019-04-21 18:17:02 -0400

    Update discord.html.php

--- content/pages/discord.html.php
+++ content/pages/discord.html.php
@@ -1,3 +1,3 @@
-<pre><div class="fineprint">Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Discord server</a> in the Text Channel: cosplay-channel</pre></div>
+<pre><div class="fineprint">Join us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">Discord server</a> in the Text Channel: cosplay-academy</pre></div>
 
 You'll get the opportunity to ask questions to the regulars of weekly Crawl Cosplay Challenge.
