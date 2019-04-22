d422b88189f812e4b33651fe9a6124325864b36c
RoGGa <robert_gagne@yahoo.com>
2019-04-22 16:51:08 -0400

    Update about.html.php

--- content/pages/about.html.php
+++ content/pages/about.html.php
@@ -1,2 +1,6 @@
 <h2>About Cosplay Academy</h2>
-to do
+
+CosPlay Academy is a prequel to the weekly 
+<a href="http://www.crawlcosplay.com/" target="_blank" title="Visit the CCC website">Crawl Cosplay Challenge</a> (CCC).
+
+under construction
