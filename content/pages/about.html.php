34f5847b5523f710636e8192aabb93a6a96fd861
RoGGa <robert_gagne@yahoo.com>
2019-04-22 16:54:43 -0400

    Update about.html.php

--- content/pages/about.html.php
+++ content/pages/about.html.php
@@ -1,9 +1,7 @@
 <h2>About Cosplay Academy</h2>
-
 CosPlay Academy is a prequel to the weekly 
 <a href="http://www.crawlcosplay.com/" target="_blank" title="Visit the CCC website">Crawl Cosplay Challenge</a> (CCC).
-
-The intended audience are DCSS players who are fairly new to the game and don't feel they have the skills necessary yet 
+<p>The intended audience are DCSS players who are fairly new to the game and don't feel they have the skills necessary yet 
 to compete in the Crawl Cosplay Challenge at a level that isn't just beginner.
 
 -- under construction --
