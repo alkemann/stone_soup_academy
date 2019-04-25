3b80e385598c5dd71e6ed442cc26e03578566328
RoGGa <robert_gagne@yahoo.com>
2019-04-25 18:34:45 -0400

    Update hybrid.html.php

--- content/pages/hybrid.html.php
+++ content/pages/hybrid.html.php
@@ -1,4 +1,4 @@
 <h2>Hybrid: Melee & Magic</h2>
 
-<img src="/img/Grum.png" height="48" width="48"> <a href="/uniques/grum" target="_blank">Grum</a>, the Gnoll Summoner (GnGl)
+<img src="/img/Grum.png" height="48" width="48"> <a href="/uniques/grum">Grum</a>, the Gnoll Summoner (GnGl)
 
