27759419180fde9ba9a53d19e4233ba20b4797ff
RoGGa <robert_gagne@yahoo.com>
2019-04-25 15:08:42 -0400

    Update about.html.php

--- content/pages/about.html.php
+++ content/pages/about.html.php
@@ -1,9 +1,8 @@
 <h2>About Cosplay Academy</h2>
-Crawl's CosPlay Academy (CCA) is a prequel to the weekly 
-<a href="http://www.crawlcosplay.com/" target="_blank" title="Visit the CCC website">Crawl Cosplay Challenge</a> (CCC).
+Crawl's CosPlay Academy (CCA) is intended as a prequel to the weekly 
+<a href="http://www.crawlcosplay.com/" target="_blank" title="Visit the CCC website">Crawl Cosplay Challenge</a> (CCC)...and yet, on its own, it is so much more
 <p>The intended audience is <abbr title="Dungeon Craw: Stone Soup">DCSS</abbr> players who are fairly new to the game or haven't won at least 4 games with each of the different styled combos.
-It's also for players who don't feel they have the skills necessary yet to compete in the Crawl Cosplay Challenge at a level that isn't just beginner.
-
+As a prequel, it's for players who don't feel they have the skills necessary yet to compete in the Crawl Cosplay Challenge at a level that isn't just beginner.
 
 <h4>Certification Process</h4>
 As you complete a game with either a win or <abbr title="Yet Another Sad Death">YASD</abbr>, you submit your morgue to track your progress.
