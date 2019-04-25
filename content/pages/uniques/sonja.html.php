0de48e27cbfacd691dd6e143a5f74df3b3ed3371
RoGGa <robert_gagne@yahoo.com>
2019-04-25 19:12:21 -0400

    Create sonja.html.php

---
+++ content/pages/uniques/sonja.html.php
@@ -0,0 +1,9 @@
+<h2><img src="/img/Sonja.png" height="48" width="48"> <a href="http://crawl.chaosforge.org/Sonja" target="_blank">Sonja</a>: "The Royal Ballet doesn't accept kobolds. That's why I went into killing people."</h2></td></tr>
+<blockquote><blockquote><blockquote><i>
+A graceful kobold assassin. Admire her graceful pirouettes, thrill at her daring leaps! But watch out for her knives!
+</i></blockquote></blockquote></blockquote>
+<img src="/img/HR-right.png"><br />
+
+<h2>Character Guide</h2>
+<p>As a <a href="http://crawl.chaosforge.org/Kobold" target="_blank">Kobold</a> 
+        <a href="http://crawl.chaosforge.org/Assassin"  target="_blank">Assassin</a> (KoAs)...</p>
