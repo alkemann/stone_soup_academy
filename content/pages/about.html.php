f4ad95008f8069eb9515162d37002146d22fee7a
RoGGa <robert_gagne@yahoo.com>
2019-04-25 14:49:44 -0400

    Update about.html.php

--- content/pages/about.html.php
+++ content/pages/about.html.php
@@ -1,8 +1,24 @@
 <h2>About Cosplay Academy</h2>
-CosPlay Academy is a prequel to the weekly 
+Crawl's CosPlay Academy (CCA) is a prequel to the weekly 
 <a href="http://www.crawlcosplay.com/" target="_blank" title="Visit the CCC website">Crawl Cosplay Challenge</a> (CCC).
-<p>The intended audience is DCSS players who are fairly new to the game and don't feel they have the skills necessary yet 
-to compete in the Crawl Cosplay Challenge at a level that isn't just beginner.
+<p>The intended audience is DCSS players who are fairly new to the game or haven't won at least 4 games with each of the different styled combos.
+It's also for players who don't feel they have the skills necessary yet to compete in the Crawl Cosplay Challenge at a level that isn't just beginner.
+
+<h3>Certification Process</h3>
+As you complete a game with either a win or <abbr="Yet Another Sad Death">YASD</abbr>, you submit your morgue to track your progress.
+If you did not win the game, you get assigned a ghost XL title from the following:
+
+<table>
+  <tr><th>XL</th><th>Title</th></tr>
+  <tr><th> 3</th><th>Weakling</th></tr>
+  <tr><th> 6</th><th>Amateur</th></tr>
+  <tr><th> 9</th><th>Novice</th></tr>
+  <tr><th>12</th><th>Journeyman</th></tr>
+  <tr><th>15</th><th>Adept</th></tr>
+  <tr><th>18</th><th>Veteran</th></tr>
+  <tr><th>21</th><th>Master</th></tr>
+  <tr><th>24+</th><th>Legendary</th></tr>
+<table>
 
 -- under construction --
   <p>You'll also be added to the <a href="/scoreboard">Honour Roll</a> allowing you to see also the accomplishment of other crawlers.</p>
