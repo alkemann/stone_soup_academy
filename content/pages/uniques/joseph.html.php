9353c9b44564858f66846780af43d0053d3be094
RoGGa <robert_gagne@yahoo.com>
2019-04-25 19:25:38 -0400

    Create joseph.html.php

---
+++ content/pages/uniques/joseph.html.php
@@ -0,0 +1,10 @@
+<h2><img src="/img/Joseph.png" height="48" width="48"> <a href="http://crawl.chaosforge.org/Joseph" target="_blank">Joseph</a>: 
+    "It's nothing personal, but I have to kill you."</h2></td></tr>
+<blockquote><blockquote><blockquote><i>
+Looks like a mercenary in a stupid-looking hat.
+</i></blockquote></blockquote></blockquote>
+<img src="/img/HR-right.png"><br />
+
+<h2>Character Guide</h2>
+<p>As a <a href="http://crawl.chaosforge.org/Halfling" target="_blank">Halfling</a> 
+        <a href="http://crawl.chaosforge.org/Hunter"  target="_blank">Hunter</a> (HaHu)...</p>
