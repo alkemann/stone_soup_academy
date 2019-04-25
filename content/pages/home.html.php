054865fc865aadf6cd5d442c53ee30d9941e0d90
RoGGa <robert_gagne@yahoo.com>
2019-04-25 12:23:40 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -7,7 +7,9 @@
 <p>This will also allow you to:</p>
     <li>better compete in the weekly <a href="http://www.crawlcosplay.com/">Crawl Cosplay Challenge</a>.</li>
 
-<h4>Ready? Set, GO!</h4>
+<h4>Ready?</h4>
+Want to know more before jumping in? See the <a href="/about">About</a> section.    
+<h4>Set, GO!</h4>
 Your first Academy goal is to play one of the "melee brutes" listed below since they are considered to be <b><i>easier</i></b> of their category.</p>
 <p>Once you have a "good" game, <a href="/submit">Submit your Morque</a> to gain access to porfolio</a>.
 <p>You'll also be added to the <a href="/scoreboard">Honour Roll</a> allowing you to see also the accomplishment of other crawlers.</p>
