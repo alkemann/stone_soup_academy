23f2b869c7d92e46a673dcff55e6b9959425f633
RoGGa <robert_gagne@yahoo.com>
2019-04-25 18:44:37 -0400

    Create ilsuiw.html.php

---
+++ content/pages/uniques/ilsuiw.html.php
@@ -0,0 +1,10 @@
+<h2><img src="/img/Ilsuiw.png" height="48" width="48"> <a href="http://crawl.chaosforge.org/Ilsuiw" target="_blank">Ilsuiw</a>: 
+  "To sleep, to sleep... mayhap to drown."</h2></td></tr>
+<blockquote><blockquote><blockquote><i>
+A deadly merfolk water-witch, who uses her dominion over the tides to draw unwitting adventurers to their deaths.
+</i></blockquote></blockquote></blockquote>
+<img src="/img/HR-right.png"><br />
+
+<h2>Character Guide</h2>
+<p>As a <a href="http://crawl.chaosforge.org/Merfolk" target="_blank">Gnoll</a> 
+        <a href="http://crawl.chaosforge.org/Skald"  target="_blank">Skald</a> (MfSk)...</p>
