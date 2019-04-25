9803c6671a37f0cc7bf80c4af1a8dc8d9c0fb82d
RoGGa <robert_gagne@yahoo.com>
2019-04-25 14:07:26 -0400

    Update asterion.html.php

--- content/pages/uniques/asterion.html.php
+++ content/pages/uniques/asterion.html.php
@@ -1,7 +1,7 @@
 <h2><img src="/img/Asterion.png" height="48" width="48"> <a href="http://crawl.chaosforge.org/Asterion">Asterion</a>: "Makhleb has given me powers of destruction."</h2></td></tr>
-<blockquote><i>Once a king infamous for waging bloodthirsty campaigns, Asterion was exiled to the depths of the dungeon for his crimes.
+<blockquote><blockquote><i>Once a king infamous for waging bloodthirsty campaigns, Asterion was exiled to the depths of the dungeon for his crimes.
 Here he lurks in his tattered finery, dreaming of a new kingdom. In his lust for power, the minotaur king turned to the worship
-of Makhleb, and Makhleb has rewarded him with horrifying destructive might.</i></blockquote>
+of Makhleb, and Makhleb has rewarded him with horrifying destructive might.</i></blockquote></blockquote>
 <img src="/img/HR-right.png"><br />
 
 <h2>Character Guide</h2>
