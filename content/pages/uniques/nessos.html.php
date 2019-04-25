1eef19bba540bef5117b831bc6df24b75ad56eb4
RoGGa <robert_gagne@yahoo.com>
2019-04-25 17:26:11 -0400

    Create nessos.html.php

---
+++ content/pages/uniques/nessos.html.php
@@ -0,0 +1,8 @@
+<h2><img src="/img/Nessos.png" height="48" width="48"> <a href="http://crawl.chaosforge.org/Nessos">Nessos</a>: "I'm half man! Half horse! And all stone-cold KILLER!"</h2></td></tr>
+<blockquote><i>
+A centaur warrior, skilled at warfare and deceit. He slew a mighty hydra, and now its deadly blood drips, still-fuming, from his arrows.
+</i></blockquote>
+<img src="/img/HR-right.png"><br />
+
+<h2>Character Guide</h2>
+<p>As a <a href="http://crawl.chaosforge.org/Centaur">Centaur</a> <a href="http://crawl.chaosforge.org/Hunter">Hunter</a> (CeHu)...</p>
