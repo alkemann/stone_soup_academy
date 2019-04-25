712184611750309c81334803d2df6833608ac033
RoGGa <robert_gagne@yahoo.com>
2019-04-25 19:49:37 -0400

    Update neck.html.php

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -9,13 +9,14 @@
             <h1><a href="/"><code>Crawl Cosplay Academy</code></a> 
                 <a href="/uniques/snorg" title="Snorg"><img src="/img/Snorg.png"></a> 
                 <a href="/uniques/asterion" title="Asterion"><img src="/img/Asterion.png"></a> 
-                <a href="/uniques/saint-roka" title="Saint Roka"><img src="/img/Saint_Roka.png"></a> 
+                <a href="/uniques/saint-roka" title="Saint Roka"><img src="/img/Saint_Roka.png"></a> - 
                 <a href="/uniques/nessos" title="Nessos"><img src="/img/Nessos.png"></a> 
                 <a href="/uniques/sonya" title="Sonja"><img src="/img/Sonja.png"></a> 
-                <a href="/uniques/joseph" title="Joseph"><img src="/img/Joseph.png"></a> 
+                <a href="/uniques/joseph" title="Joseph"><img src="/img/Joseph.png"></a> - 
                 <a href="/uniques/grum" title="Grum"><img src="/img/Grum.png"></a> 
                 <a href="/uniques/ilsuiw" title="Ilsuiw"><img src="/img/Ilsuiw.png"></a> 
-                <a href="/uniques/jorgrun" title="Jorgrun"><img src="/img/Jorgrun.png"></a> 
+                <a href="/uniques/jory" title="Jory"><img src="/img/Jory.png"></a> - 
+                <a href="/uniques/jorgrun" title="Jorgrun"><img src="/img/Jorgrun.png"></a>   
                 <a href="/uniques/fannar" title="Fannar"><img src="/img/Fannar.png"></a> 
                 <a href="/uniques/josephine" title="Josephine"><img src="/img/Josephine.png"></a> 
             </h1>
