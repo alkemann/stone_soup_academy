d03fda29cd16f27b943de1530632e8533bf73b24
RoGGa <robert_gagne@yahoo.com>
2019-04-25 17:12:39 -0400

    Delete ranking-system.html.php

--- content/pages/ranking-system.html.php
+++
@@ -1,4 +0,0 @@
-<h4>Ranking System based on Ghost Player ranks</h4>
-<p />
-( taken from:  <a href="https://github.com/crawl/crawl/blob/master/crawl-ref/source/describe.cc#L4661">https://github.com/crawl/crawl/blob/master/crawl-ref/source/describe.cc#L4661</a> )<p />
-
