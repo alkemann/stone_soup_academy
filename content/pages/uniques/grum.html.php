be05f7f8e0e04e047c3ef2fa41018726f20dc47b
RoGGa <robert_gagne@yahoo.com>
2019-04-25 18:12:06 -0400

    Create grum.html.php

---
+++ content/pages/uniques/grum.html.php
@@ -0,0 +1,9 @@
+<h2><img src="/img/grum.png" height="48" width="48"> <a href="http://crawl.chaosforge.org/Grum" target="_blank">Grum</a>: "I'll hound you night and day!"</h2></td></tr>
+<blockquote><blockquote><blockquote><i>
+
+</i></blockquote></blockquote></blockquote>
+<img src="/img/HR-right.png"><br />
+
+<h2>Character Guide</h2>
+<p>As a <a href="http://crawl.chaosforge.org/Gnoll" target="_blank">Gnoll</a> 
+        <a href="http://crawl.chaosforge.org/Summuner"  target="_blank">Summuner</a> (GnSu)...</p>
