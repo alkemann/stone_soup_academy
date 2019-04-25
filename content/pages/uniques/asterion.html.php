89ac78fa0df6617e360d876a7a79e48730041daf
RoGGa <robert_gagne@yahoo.com>
2019-04-25 17:45:08 -0400

    Update asterion.html.php

--- content/pages/uniques/asterion.html.php
+++ content/pages/uniques/asterion.html.php
@@ -1,8 +1,9 @@
-<h2><img src="/img/Asterion.png" height="48" width="48"> <a href="http://crawl.chaosforge.org/Asterion">Asterion</a>: "Makhleb has given me powers of destruction."</h2></td></tr>
+<h2><img src="/img/Asterion.png" height="48" width="48"> <a href="http://crawl.chaosforge.org/Asterion" target="_blank">Asterion</a>: "Makhleb has given me powers of destruction."</h2></td></tr>
 <blockquote><blockquote><blockquote><i>Once a king infamous for waging bloodthirsty campaigns, Asterion was exiled to the depths of the
 dungeon for his crimes. Here he lurks in his tattered finery, dreaming of a new kingdom. In his lust for power, the minotaur king turned
 to the worship of Makhleb, and Makhleb has rewarded him with horrifying destructive might.</i></blockquote></blockquote></blockquote>
 <img src="/img/HR-right.png"><br />
 
 <h2>Character Guide</h2>
-<p>As a <a href="http://crawl.chaosforge.org/Minortaur">Minotaur</a> <a href="http://crawl.chaosforge.org/Fighter">Fighter</a> (MiFi) of Makhleb...</p>
+<p>As a <a href="http://crawl.chaosforge.org/Minortaur" target="_blank">Minotaur</a> 
+        <a href="http://crawl.chaosforge.org/Fighter" target="_blank">Fighter</a> (MiFi) of Makhleb...</p>
