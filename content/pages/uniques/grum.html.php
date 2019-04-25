658da6f6e099697f59cf90c8faabeba70e956616
RoGGa <robert_gagne@yahoo.com>
2019-04-25 18:25:43 -0400

    Update grum.html.php

--- content/pages/uniques/grum.html.php
+++ content/pages/uniques/grum.html.php
@@ -1,6 +1,6 @@
 <h2><img src="/img/Grum.png" height="48" width="48"> <a href="http://crawl.chaosforge.org/Grum" target="_blank">Grum</a>: "I'll hound you night and day!"</h2></td></tr>
 <blockquote><blockquote><blockquote><i>
-
+A tough-looking gnoll, wearing the pelt of one of his former war dogs.
 </i></blockquote></blockquote></blockquote>
 <img src="/img/HR-right.png"><br />
 
