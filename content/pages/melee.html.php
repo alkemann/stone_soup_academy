f0295efe32a2df26c36c202c31d883118f7bd286
RoGGa <robert_gagne@yahoo.com>
2019-04-25 17:14:55 -0400

    Create melee.html.php

---
+++ content/pages/melee.html.php
@@ -0,0 +1,6 @@
+<h3>Guides and conducts for "Simple" melee characters:</h3>
+<ul class="charlist">
+    <li><a href="/uniques/snorg"><img src="/img/Snorg.png"> Snorg</a>, the <u><b>Tr</b></u>oll <u><b>Be</b></u>rzerker (<abbr title="Troll Berzerker">TrBe</abbr>) of Trog</li>
+    <li><a href="/uniques/asterion"><img src="/img/Asterion.png"> Asterion</a>, the <u><b>Mi</b></u>notaur <u><b>Fi</b></u>ghter (<abbr title="Minotaur Fighter">MiFi</abbr>) of Makhleb</li>
+    <li><a href="/uniques/saint-roka"><img src="/img/Saint_Roka.png"> Saint Roka</a>, the <u><b>H</b></u>ill <u><b>O</b></u>rc <u><b>Mo</b></u>nk (<abbr title="Hill Orc Monk">HOMo</abbr>) of Beogh (beginner-intermediate level)</li>
+</ul>
