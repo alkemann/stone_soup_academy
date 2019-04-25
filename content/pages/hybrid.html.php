1e2af5618b1b7f030d1d0153f3e6f492064e905a
RoGGa <robert_gagne@yahoo.com>
2019-04-25 18:30:57 -0400

    Update hybrid.html.php

--- content/pages/hybrid.html.php
+++ content/pages/hybrid.html.php
@@ -1,3 +1,4 @@
-Hybrid
+<h2>Hybrid: Melee & Magic</h2>
 
-Grum, the Gnoll Gladiator (GnGl) of Yredelemnul
+<img src="/img/Grum.png" height="48" width="48"> <a href="/uniques/grum" target="_blank">Grum</a>, the Gnoll Summoner (GnGl)
+
