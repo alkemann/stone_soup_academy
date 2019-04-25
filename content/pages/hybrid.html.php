92c11eb4eed235cca5a0d6d9adc5587ca5a6dd6e
RoGGa <robert_gagne@yahoo.com>
2019-04-25 18:39:03 -0400

    Update hybrid.html.php

--- content/pages/hybrid.html.php
+++ content/pages/hybrid.html.php
@@ -1,4 +1,5 @@
 <h2>Hybrid: Melee & Magic</h2>
 
 <img src="/img/Grum.png"> <a href="/uniques/grum">Grum</a>, the Gnoll Summoner (GnGl)
+<img src="/img/Ilsuiw.png"> <a href="/uniques/grum">Ilsuiw</a>, the Merfolk Skald (MeSk)
 
