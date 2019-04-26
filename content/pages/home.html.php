a79dc2d0a5b8fe5146f1cf265c87bbb531180942
RoGGa <robert_gagne@yahoo.com>
2019-04-26 13:39:38 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -1,16 +1,16 @@
 <h2>Welcome Crawler!</h2>
-<p><img src="/img/Snorg.png" style="float:right;width:48px;height:48px;">Your mission, if you choose to accept it, is to become certified
-with the Crawl Cosplay Academy (CCA). While doing so, you'll play fun combos as you learn to survive longer, and hopefully win many games
-sooner than later. So buckle up with <abbr title="Crawl Cosplay Academy">CCA</abbr> as we provide you with a structured approach to ease 
-the learning curve while playing characters based on <a href="http://crawl.chaosforge.org/Unique_monster" target="_blank">DCSS Uniques</a>
-such as our maskot, <b><i>Snorg</b></i>:
+<p><img src="/img/Snorg.png" style="float:right;width:48px;height:48px;">Want to improve your gameplay while having fun doing it? 
+Just join for free the Crawl Cosplay Academy (CCA) to make that happen. While doing so, you'll play fun combos as you learn to 
+survive longer, and hopefully win many games sooner than later. So buckle up with <abbr title="Crawl Cosplay Academy">CCA</abbr> 
+as we provide you with a structured approach to ease the learning curve while playing characters based on 
+<a href="http://crawl.chaosforge.org/Unique_monster" target="_blank">DCSS Uniques</a> such as our maskot, <b><i>Snorg</b></i>:
 <h4>Ready?</h4>
 Want to know more before jumping in? Read the <a href="/about">About</a> section.
-<h4>Set, GO!</h4>
+<h4>Set...</h4>
 If you have never won a game, your first Academy goal is to play one of the "melee brutes" listed below since they are considered to be the
 <b><i>easier</i></b> combos of the easiest category. Once you have completed one of your better games, <a href="/submit">Submit your Morque</a> to gain access to your porfolio</a>.
 <p>If you are fairly new to DCSS, the following character guides should be very helpful.</p>
-<h3>Guides and conducts for "Simple" melee characters:</h3>
+<h3>GO! Guides and conducts for "Simple" melee characters:</h3>
 <ul class="charlist">
     <li><a href="/uniques/snorg"><img src="/img/Snorg.png"> Snorg</a>, the <u><b>Tr</b></u>oll <u><b>Be</b></u>rzerker (<abbr title="Troll Berzerker">TrBe</abbr>) of Trog</li>
     <li><a href="/uniques/asterion"><img src="/img/Asterion.png"> Asterion</a>, the <u><b>Mi</b></u>notaur <u><b>Fi</b></u>ghter (<abbr title="Minotaur Fighter">MiFi</abbr>) of Makhleb</li>
