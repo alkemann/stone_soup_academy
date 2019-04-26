62e03118e9dad50bffe2f30a7d3c8628a27ec98f
RoGGa <robert_gagne@yahoo.com>
2019-04-26 13:55:41 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -6,16 +6,12 @@ as we provide you with a structured approach to ease the learning curve while pl
 <a href="http://crawl.chaosforge.org/Unique_monster" target="_blank">DCSS Uniques</a> such as our maskot, <b><i>Snorg</b></i>:
 <h4>Ready?</h4>
 Want to know more before jumping in? Read the <a href="/about">About</a> section.
-<h4>Set...</h4>
-If you have never won a game, your first Academy goal is to play one of the "melee brutes" listed below since they are considered to be the
-<b><i>easier</i></b> combos of the easiest category. Once you have completed one of your better games, <a href="/submit">Submit your Morque</a> to gain access to your porfolio</a>.
+<h4>Set...GO!</h4>
+If you have never won a game, your first Academy goal is to play one of the "melee brutes" listed above since they are considered to be the
+<b><i>easier</i></b> combos of the easiest category. 
+<h3>Submit your morgues</h3>
+Once you have completed one of your better games, <a href="/submit">Submit your Morque</a> to gain access to your porfolio</a>.
 <p>If you are fairly new to DCSS, the following character guides should be very helpful.</p>
-<h3>GO! Guides and conducts for "Simple" melee characters:</h3>
-<ul class="charlist">
-    <li><a href="/uniques/snorg"><img src="/img/Snorg.png"> Snorg</a>, the <u><b>Tr</b></u>oll <u><b>Be</b></u>rzerker (<abbr title="Troll Berzerker">TrBe</abbr>) of Trog</li>
-    <li><a href="/uniques/asterion"><img src="/img/Asterion.png"> Asterion</a>, the <u><b>Mi</b></u>notaur <u><b>Fi</b></u>ghter (<abbr title="Minotaur Fighter">MiFi</abbr>) of Makhleb</li>
-    <li><a href="/uniques/saint-roka"><img src="/img/Saint_Roka.png"> Saint Roka</a>, the <u><b>H</b></u>ill <u><b>O</b></u>rc <u><b>Mo</b></u>nk (<abbr title="Hill Orc Monk">HOMo</abbr>) of Beogh (beginner-intermediate level)</li>
-</ul>
 <h3>Need more help?</h3>
 <p>Come chat with us on our <a href="https://discord.gg/ZQ4kk6n" target="_blank">CosPlay Discord server</a> in the #cosplay-academy text channel.
 <p>Want to do a bit of the legwork on your own, then read the following:
