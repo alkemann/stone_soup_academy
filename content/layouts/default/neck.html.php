476a9035a6459e2607b0185ae20c168619af90db
RoGGa <robert_gagne@yahoo.com>
2019-04-25 20:36:59 -0400

    Update neck.html.php

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -21,5 +21,5 @@
                 <a href="/uniques/josephine" title="Josephine, the DDNe"><img src="/img/Josephine.png"></a> 
             </h1>
         </div>
-        <pre><img src="/img/HR-right-short.png">         <a href="/melee">Melee</a>            <a href="/ranged">Ranged</a>            <a href="/hybrid">Hybrid</a>             <a href="/magic>Magic</a></pre>
+        <pre><img src="/img/HR-right-short.png">         <a href="/melee">Melee</a>            <a href="/ranged">Ranged</a>            <a href="/hybrid">Hybrid</a>             <a href="/magic">Magic</a></pre>
     <div class="content">
