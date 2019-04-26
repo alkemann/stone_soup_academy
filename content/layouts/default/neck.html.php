c2bda97ba3b846d7cd0b1dc681a871a9c21aabe6
RoGGa <robert_gagne@yahoo.com>
2019-04-25 20:17:07 -0400

    Update neck.html.php

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -21,5 +21,5 @@
                 <a href="/uniques/josephine" title="Josephine, the DDNe"><img src="/img/Josephine.png"></a> 
             </h1>
         </div>
-        <img src="/img/HR-right.png"><br />
+        <img src="/img/HR-right-short.png"><br />
     <div class="content">
