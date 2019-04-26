67ae8b9f8ef65eca2189188756425c0534abc75c
RoGGa <robert_gagne@yahoo.com>
2019-04-25 20:20:43 -0400

    Update neck.html.php

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -21,5 +21,5 @@
                 <a href="/uniques/josephine" title="Josephine, the DDNe"><img src="/img/Josephine.png"></a> 
             </h1>
         </div>
-        <img src="/img/HR-right-short.png"><br /> Melee - Ranged - Hybrid - Magic
+        <img src="/img/HR-right-short.png"> Melee - Ranged - Hybrid - Magic  <br />
     <div class="content">
