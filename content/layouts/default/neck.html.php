e0aa9136309a2c0d456c2cadfc29feb0607a726d
RoGGa <robert_gagne@yahoo.com>
2019-04-26 17:43:19 -0400

    Update neck.html.php

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -10,12 +10,12 @@
                 <a href="/uniques/snorg" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a> 
                 <a href="/uniques/saint-roka" title="Saint Roka, the Hill Orc Monk (HOMo)"><img src="/img/Saint_Roka.png"></a> - 
                 <a href="/uniques/nessos" title="Nessos, the Centaur Hunter (CeHu)"><img src="/img/Nessos.png"></a> 
-                <a href="/uniques/sonya" title="Sonja, the Kobole Assassin (KoAs)"><img src="/img/Sonja.png"></a> 
+                <a href="/uniques/sonya" title="Sonja, the Kobole Assassin (KoAs)"><img src="/img/Sonja.png"></a> - 
                 <a href="/uniques/grum" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/Grum.png"></a> 
                 <a href="/uniques/jory" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/Jory.png"></a> - 
                 <a href="/uniques/tiamat" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat.png"></a>   
                 <a href="/uniques/josephine" title="Josephine, the Deep Dwarf Necromancer (DDNe)"><img src="/img/Josephine.png"></a> 
             </h1>
         </div>
-        <pre><img src="/img/HR-right-short.png">       <a href="/melee">Melee</a>       <a href="/ranged">Ranged</a>       <a href="/hybrid">Hybrid</a>       <a href="/magic">Magic</a></pre>
+        <pre><img src="/img/HR-right-short.png">         <a href="/melee">Melee</a>       <a href="/ranged">Ranged</a>       <a href="/hybrid">Hybrid</a>       <a href="/magic">Magic</a></pre>
     <div class="content">
