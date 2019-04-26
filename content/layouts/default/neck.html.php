0b3fdff8614db17bc2c7bb265c5912dbbd087026
RoGGa <robert_gagne@yahoo.com>
2019-04-25 20:02:56 -0400

    Update neck.html.php

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -7,18 +7,18 @@
     <div class="page_content">
         <div class="heading">
             <h1><a href="/"><code>Crawl Cosplay Academy</code></a> - 
-                <a href="/uniques/snorg" title="Snorg"><img src="/img/Snorg.png"></a> 
-                <a href="/uniques/asterion" title="Asterion"><img src="/img/Asterion.png"></a> 
-                <a href="/uniques/saint-roka" title="Saint Roka"><img src="/img/Saint_Roka.png"></a> - 
-                <a href="/uniques/nessos" title="Nessos"><img src="/img/Nessos.png"></a> 
-                <a href="/uniques/sonya" title="Sonja"><img src="/img/Sonja.png"></a> 
-                <a href="/uniques/joseph" title="Joseph"><img src="/img/Joseph.png"></a> - 
-                <a href="/uniques/grum" title="Grum"><img src="/img/Grum.png"></a> 
-                <a href="/uniques/ilsuiw" title="Ilsuiw"><img src="/img/Ilsuiw.png"></a> 
-                <a href="/uniques/jory" title="Jory"><img src="/img/Jory.png"></a> - 
-                <a href="/uniques/jorgrun" title="Jorgrun"><img src="/img/Jorgrun.png"></a>   
-                <a href="/uniques/fannar" title="Fannar"><img src="/img/Fannar.png"></a> 
-                <a href="/uniques/josephine" title="Josephine"><img src="/img/Josephine.png"></a> 
+                <a href="/uniques/snorg" title="Snorg, the TrBe"><img src="/img/Snorg.png"></a> 
+                <a href="/uniques/asterion" title="Asterion, the MiFi"><img src="/img/Asterion.png"></a> 
+                <a href="/uniques/saint-roka" title="Saint Roka, the HOMo"><img src="/img/Saint_Roka.png"></a> - 
+                <a href="/uniques/nessos" title="Nessos, the CeHu"><img src="/img/Nessos.png"></a> 
+                <a href="/uniques/sonya" title="Sonja, the KoAs"><img src="/img/Sonja.png"></a> 
+                <a href="/uniques/joseph" title="Joseph, the HaHu"><img src="/img/Joseph.png"></a> - 
+                <a href="/uniques/grum" title="Grum, the GnSu"><img src="/img/Grum.png"></a> 
+                <a href="/uniques/ilsuiw" title="Ilsuiw, the MfSk"><img src="/img/Ilsuiw.png"></a> 
+                <a href="/uniques/jory" title="Jory, the VpEn"><img src="/img/Jory.png"></a> - 
+                <a href="/uniques/jorgrun" title="Jorgrun, the GrEE"><img src="/img/Jorgrun.png"></a>   
+                <a href="/uniques/fannar" title="Fannar, the DEIE"><img src="/img/Fannar.png"></a> 
+                <a href="/uniques/josephine" title="Josephine, the DDNe"><img src="/img/Josephine.png"></a> 
             </h1>
         </div>
         <img src="/img/HR-right.png"><br />
