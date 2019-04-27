83e65ab83560026626bf22fd1fc47e2adcc9212f
RoGGa <robert_gagne@yahoo.com>
2019-04-27 15:56:49 -0400

    Update melee.html.php

--- content/pages/melee.html.php
+++ content/pages/melee.html.php
@@ -1,6 +1,16 @@
-<h3>Guides and conducts for "Simple" melee characters:</h3>
+<h3>Conducts for "Simple" melee characters:</h3>
 <ul class="charlist">
-    <li><a href="/uniques/snorg"><img src="/img/Snorg.png"> Snorg</a>, the <u><b>Tr</b></u>oll <u><b>Be</b></u>rzerker (<abbr title="Troll Berzerker">TrBe</abbr>) of Trog</li>
-    <li><a href="/uniques/asterion"><img src="/img/Asterion.png"> Asterion</a>, the <u><b>Mi</b></u>notaur <u><b>Fi</b></u>ghter (<abbr title="Minotaur Fighter">MiFi</abbr>) of Makhleb</li>
-    <li><a href="/uniques/saint-roka"><img src="/img/Saint_Roka.png"> Saint Roka</a>, the <u><b>H</b></u>ill <u><b>O</b></u>rc <u><b>Mo</b></u>nk (<abbr title="Hill Orc Monk">HOMo</abbr>) of Beogh (beginner-intermediate level)</li>
+    <li><img src="/img/Snorg.png"> Snorg, the <u><b>Tr</b></u>oll <u><b>Be</b></u>rzerker (<abbr title="Troll Berzerker">TrBe</abbr>) of Trog</li>
+        <ol><li>Turn on manual training for Fighting, Unarmed Combat (UC), and dodging for the whole game.</li>
+            <li>Select STR at every 3rd level.</li>
+            <li>Use a shield when you find one, and turn on Shield training.</li>
+            <li>Don't abandon Trog for a 3 rune win.</li></ol>
+    <li><img src="/img/Asterion.png"> Asterion, the <u><b>Mi</b></u>notaur <u><b>Fi</b></u>ghter (<abbr title="Minotaur Fighter">MiFi</abbr>) of Makhleb</li>
+    
+    <li><img src="/img/Saint_Roka.png"> Saint Roka, the <u><b>H</b></u>ill <u><b>O</b></u>rc <u><b>Mo</b></u>nk (<abbr title="Hill Orc Monk">HOMo</abbr>) of Beogh (beginner-intermediate level)</li>
+        <ol><li>Turn on manual training for Fighting and dodging for the whole game.</li>
+            <li>Train Axes to mindelay (14+).</li>
+            <li>Select STR at every 3rd level.</li>
+            <li>Don't abandon Beogh for a 3 rune win.</li>
+            <li>Read UV4's guide for HOMo</li></ol>
 </ul>
