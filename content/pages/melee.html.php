feba17a7d7f60879100ece14841a2eb4071a66df
RoGGa <robert_gagne@yahoo.com>
2019-04-29 17:02:42 -0400

    Update melee.html.php

--- content/pages/melee.html.php
+++ content/pages/melee.html.php
@@ -1,17 +1,16 @@
 <h3>Conducts for "Simple" melee characters:</h3>
-<ul class="charlist">
-    <li><img src="/img/Snorg.png"> Snorg, the <u><b>Tr</b></u>oll <u><b>Be</b></u>rzerker (<abbr title="Troll Berzerker">TrBe</abbr>) of Trog</li>
+<ul class="charlist"><li><img src="/img/Snorg.png"> Snorg, the <u><b>Tr</b></u>oll <u><b>Be</b></u>rzerker (<abbr title="Troll Berzerker">TrBe</abbr>) of Trog</li></ul>
         <ol><li>Turn on manual training for Fighting, Unarmed Combat (UC), and dodging for the whole game.</li>
             <li>Select STR at every 3rd level, unless INT falls below 3.</li>
             <li>Use a shield when you find one, and turn on Shield training.</li>
             <li>Don't abandon Trog for a 3 rune win.</li></ol>
-    <li><img src="/img/Asterion.png"> Asterion, the <u><b>Mi</b></u>notaur <u><b>Fi</b></u>ghter (<abbr title="Minotaur Fighter">MiFi</abbr>) of Makhleb</li>
+<ul class="charlist"><li><img src="/img/Asterion.png"> Asterion, the <u><b>Mi</b></u>notaur <u><b>Fi</b></u>ghter (<abbr title="Minotaur Fighter">MiFi</abbr>) of Makhleb</li></ul>
         <ol><li>Turn on manual training for Fighting and dodging for the whole game.</li>
             <li>Train melee weapon skill to mindelay (10+).</li>
             <li>Select STR at every 3rd level.</li>
             <li>Worship Makhleb</li>
             <li>Don't abandon Makhleb for a 3 rune win.</li></ol>
-    <li><img src="/img/Saint_Roka.png"> Saint Roka, the <u><b>H</b></u>ill <u><b>O</b></u>rc <u><b>Mo</b></u>nk (<abbr title="Hill Orc Monk">HOMo</abbr>) of Beogh (beginner-intermediate level)</li>
+<ul class="charlist"><li><img src="/img/Saint_Roka.png"> Saint Roka, the <u><b>H</b></u>ill <u><b>O</b></u>rc <u><b>Mo</b></u>nk (<abbr title="Hill Orc Monk">HOMo</abbr>) of Beogh (beginner-intermediate level)</li></ul>
         <ol><li>Choose Axe as starting melee weapon</li>
             <li>Turn on manual training for Fighting and dodging for the whole game.</li>
             <li>Train on focus (*) Axes to mindelay (14+).</li>
@@ -19,4 +18,4 @@
             <li>Follow Beogh when you meet an Orc Priest (green robe orc)</li>
             <li>Don't abandon Beogh for a 3 rune win.</li>
             <li>Read <a href="http://www.ultraviolent4.com/characters/homo.html" target="_blank" >UV4's guide for HIll Orc Monk of Beogh</a></li></ol>
-</ul>
+
