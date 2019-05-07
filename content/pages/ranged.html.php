165ed3955229e305e367de6c7d21529712f0b302
RoGGa <robert_gagne@yahoo.com>
2019-05-07 18:09:24 -0400

    Update ranged.html.php

--- content/pages/ranged.html.php
+++ content/pages/ranged.html.php
@@ -2,14 +2,19 @@
 <ul class="charlist"><li><img src="/img/Nessos.png"> <a href="http://crawl.chaosforge.org/Nessos" target="_blank">Nessos</a>, 
             the <a href="http://crawl.chaosforge.org/Centaur" target="_blank">Centaur</a> 
             <a href="http://crawl.chaosforge.org/Hunter" target="_blank">Hunter</a>
-            (<abbr title="Centaur Hunter">CeHu</abbr>)
+            (<abbr title="Centaur Hunter">CeHu</abbr>)</li></ul>
 <blockquote><blockquote>
             <ol><li>Select Bows at character creation.</li>
                 <li>Turn on manual training for Fighting, and Dodging for the whole game.</li>
                 <li>Select STR at every 3rd level.</li>
-             <li>Recommended Gods: Okaruwa</li></ol>
+                <li>Recommended God(s): Okawaru</li></ol>
 </blockquote></blockquote>
 <ul class="charlist"><li><img src="/img/Joseph.png"> <a href="http://crawl.chaosforge.org/Joseph" target="_blank">Nessos</a>, 
             the <a href="http://crawl.chaosforge.org/Halfling" target="_blank">Halfling</a> 
             <a href="http://crawl.chaosforge.org/Hunter" target="_blank">Hunter</a>
-            (<abbr title="Halfling Hunter">HaHu</abbr>)
+            (<abbr title="Halfling Hunter">HaHu</abbr>)</li></ul>
+<blockquote><blockquote>
+            <ol><li>Select Sling at character creation.</li>
+                <li>Turn on manual training for Fighting, and Dodging for the whole game.</li>
+                <li>Select STR at every 3rd level.</li>
+                <li>Recommended God(s): Okawaru</li></ol>
