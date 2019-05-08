6f665acaca41c9124b00fc0b0a01c8e6d5c89030
RoGGa <robert_gagne@yahoo.com>
2019-05-08 15:34:36 -0400

    Update hybrid.html.php

--- content/pages/hybrid.html.php
+++ content/pages/hybrid.html.php
@@ -1,5 +1,13 @@
-<h2>Hybrid: Melee & Magic</h2>
+<h2>Conducts for Hybrid: Melee & Magic</h2>
 
-<img src="/img/Grum.png"> <a href="/uniques/grum">Grum</a>, the Gnoll Summoner (GnGl)
+<ul class="charlist"><li><img src="/img/Grum.png"> <a href="http://crawl.chaosforge.org/Grum" target="_blank">Grum</a>, 
+            the <a href="http://crawl.chaosforge.org/Gnoll" target="_blank">Gnoll</a> 
+            <a href="http://crawl.chaosforge.org/Summoner" target="_blank">Summoner</a>
+            (<abbr title="Gnoll Summoner">GnSu</abbr>)</li></ul>
+<blockquote><blockquote>
+            <ol><li>Turn on manual training for Fighting and Dodging for the whole game.</li>
+            <li>Select STR or INT at every 3rd level.</li>
+            <li>Use a shield when you find one, and turn on Shield training.</li></ol>
+</blockquote></blockquote>
+
 <img src="/img/Ilsuiw.png"> <a href="/uniques/grum">Ilsuiw</a>, the Merfolk Skald (MeSk)
-
