13f6bc2cd061cbb55afdf51310e6e47d9a0df7d8
RoGGa <robert_gagne@yahoo.com>
2019-05-08 18:26:34 -0400

    Update neck.html.php

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -6,12 +6,12 @@
 <div class="page information">
     <div class="page_content">
         <div class="heading">
-            <h1><img src="/img/CCA.png"> &nbsp 
-                <a href="/melee" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a><a href="/melee" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/Asterion.png"></a><a href="/melee" title="Saint Roka, the Hill Orc Monk (HOMo)"><img src="/img/Saint_Roka.png"></a> &nbsp
-                <a href="/ranged" title="Nessos, the Centaur Hunter (CeHu)"><img src="/img/Nessos.png"></a><a href="/ranged" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/Joseph.png"></a><a href="/ranged" title="Sonja, the Kobole Assassin (KoAs)"><img src="/img/Sonja.png"></a> &nbsp 
-                <a href="/hybrid" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/Grum.png"></a><a href="/hybrid" title="Ilsuiw, the Merfolk Skald (MfSk)"><img src="/img/Ilsuiw.png"></a><a href="/hybrid" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/Jory.png"></a> &nbsp 
+            <h1><img src="/img/CCA.png">
+                <a href="/melee" title="Snorg, the Troll Berzerker (TrBe)"><img src="/img/Snorg.png"></a><a href="/melee" title="Asterion, the Minotaur Fighter (MiFi)"><img src="/img/Asterion.png"></a><a href="/melee" title="Saint Roka, the Hill Orc Monk (HOMo)"><img src="/img/Saint_Roka.png"></a>&nbsp
+                <a href="/ranged" title="Nessos, the Centaur Hunter (CeHu)"><img src="/img/Nessos.png"></a><a href="/ranged" title="Joseph, the Halfling Hunter (HaHu)"><img src="/img/Joseph.png"></a><a href="/ranged" title="Sonja, the Kobole Assassin (KoAs)"><img src="/img/Sonja.png"></a>&nbsp
+                <a href="/hybrid" title="Grum, the Gnoll Summoner (GnSu)"><img src="/img/Grum.png"></a><a href="/hybrid" title="Ilsuiw, the Merfolk Skald (MfSk)"><img src="/img/Ilsuiw.png"></a><a href="/hybrid" title="Jory, the Vampire Enchanter (VpEn)"><img src="/img/Jory.png"></a>&nbsp
                 <a href="/magic" title="Tiamat, the Draconian Wizard (DrWz)"><img src="/img/Tiamat.png"></a><a href="/magic" title="Fannar, the Deep Elf Ice Elementalist (DEIE)"><img src="/img/Fannar.png"></a><a href="/magic" title="Josephine, the Deep Dwarf Necromancer (DDNe)"><img src="/img/Josephine.png"></a>
             </h1>
         </div>
-        <pre><img src="/img/HR-right-short.png">                <a href="/melee">Melee</a>             <a href="/ranged">Ranged</a>            <a href="/hybrid">Hybrid</a>             <a href="/magic">Magic</a></pre>
+        <pre><img src="/img/HR-right-short.png">              <a href="/melee">Melee</a>           <a href="/ranged">Ranged</a>          <a href="/hybrid">Hybrid</a>           <a href="/magic">Magic</a></pre>
     <div class="content">
